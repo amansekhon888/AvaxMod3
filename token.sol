74a0e22cdaea07361b3ac7824d94167c80707ee8
amansekhon888 <149692128+amansekhon888@users.noreply.github.com>
2024-03-03 12:48:24 +0530

    Update token.sol

--- token.sol
+++ token.sol
@@ -1,1 +1,29 @@
 
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract AmanToken is ERC20, ERC20Burnable, Ownable {
+    constructor(address initialOwner)
+        ERC20("AmanToken", "AMAN")
+        Ownable(initialOwner)
+    {
+        _mint(initialOwner, 1 * 10 ** decimals());
+    }
+
+    function mint(address to, uint256 amount) public onlyOwner {
+        _mint(to, amount);
+    }
+
+    function burn(address from, uint256 amount) public {
+        _burn(from, amount);
+    }
+
+    function transfer(address to, uint256 amount) public override returns (bool) {
+        _transfer(msg.sender, to, amount);
+        return true;
+    }
+}
